e77219d8664cec1b11756501a012e2dff39805d9
Sam <235806672+sampri-tech@users.noreply.github.com>
2026-02-05 22:10:09 -0400

    Mejoras
    -Struct.c
    --Se agrega el struct de tipo tiempo, tiene hora y min (minutos) ambos de tipo integer
    
    -ManejoHora.c
    -- Se agrega el struct de Tiempo
    -- Al hacer conversión a tipo Tiempo hace que a la hora de actualizarlo sea más fácil y más seguro

--- ManejoHora.c
+++ ManejoHora.c
@@ -1,33 +1,70 @@
 #include<stdio.h>
 
-int SumaHora(int hora, int suma);
+typedef struct{
+    int hora, min;
+}Tiempo;
+
+Tiempo ConvHoraCivil(int hora);
+void ActualizarTiempo(Tiempo *actulizar, Tiempo sumar);
+int ConvHoraMili(Tiempo hora);
 
 int main(){
-    int hora, y;
+    int x, y;
+    Tiempo hor1, hor2;
 
     printf("Hora: ");
-    scanf("%i", &hora);
+    scanf("%i", &x);
+
+    hor1 = ConvHoraCivil(x);
 
     printf("Suma: ");
     scanf("%i", &y);
 
-    printf("%i\n", SumaHora(hora, y));
+    hor2 = ConvHoraCivil(y);
+
+    ActualizarTiempo(&hor1, hor2);
+
+    printf("%i:%i\n", hor1.hora, hor1.min);
+
+    x = ConvHoraMili(hor1);
+
     return 0;
 }
 
-int SumaHora(int hora, int sum){
-    int aux, res;
+Tiempo ConvHoraCivil(int hora){
+    Tiempo res;
+    res.min = hora % 100;
+    res.hora = hora / 100;
 
-    res = hora + sum;
-    aux = res % 100;
+    if(res.min > 59){
+        res.hora += 1;
+        res.min -= 60;
+    }
 
-    if(aux >= 59){
-        res = (res + 100) - 59;
+    printf("%i:%i\n", res.hora, res.min);
+
+    return res;
+}
+
+void ActualizarTiempo(Tiempo *actulizar,  Tiempo sumar){
+    int totalMin;
+    totalMin = (actulizar->hora * 60) + actulizar->min  + (sumar.hora * 60) + sumar.min;
+    
+    if(totalMin > 1439){
+        totalMin -= 1440;
     }
-    aux = res % 100;
-    if(res > 2359){
-        res = aux;
-    }
+
+    actulizar->hora = (totalMin / 60) % 24;
+    actulizar->min  = totalMin % 60;
+}
+
+int ConvHoraMili(Tiempo hora){
+    int res;
+
+    res = hora.hora * 100;
+    res += hora.min;
+
+    printf("%i\n", res);
 
     return res;
 }
